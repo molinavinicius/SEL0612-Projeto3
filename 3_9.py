bcc9814d20882b253e2b4b485cc11386ab1c462f
Vinícius Molina Garcia <31550023+oervamate@users.noreply.github.com>
2020-07-12 06:09:30 -0300

    Add files via upload

---
+++ 3_9.py
@@ -0,0 +1,51 @@
+from fdtd3d import fdtd_gen
+from math import sqrt, sin, pi
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+mi0 = 4e-7 * pi
+eps0 = 8.854187817e-12
+
+c = 299792458
+
+delta = 0.01
+delta_t = 1.0005*delta/c/sqrt(2)
+
+i_max = j_max = 80
+
+coefs = [[{
+    'Ca': 1,
+	'Cb': delta_t / (eps0*delta),
+	'Da': 1,
+	'Db': delta_t / (mi0*delta)
+} for i in range(0, i_max)] for j in range(0, j_max)]
+
+gen = fdtd_gen(i_max, delta, delta_t, coefs,
+               f_meio=lambda t: sin(8e9*t))
+
+Ez, Hx, Hy = next(gen)
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 8))
+im = ax1.imshow(Ez, interpolation='none', aspect=1, vmin=-1, vmax=1)
+im2 = ax2.imshow(Hx, interpolation='none', aspect=1, vmin=-0.002, vmax=0.002)
+im3 = ax3.imshow(Hy, interpolation='none', aspect=1, vmin=-0.002, vmax=0.002)
+
+
+def update(t):
+	Ez, Hx, Hy = next(gen)
+	im.set_array(Ez)
+	im2.set_array(Hx)
+	im3.set_array(Hy)
+	return [im, im2, im3]
+
+
+anim = animation.FuncAnimation(
+	fig,
+	update,
+	frames=1240,
+	interval=30
+)
+plt.show()
+#anim.save('./onda.mp4')
+#anim.save(os.path.dirname(os.path.realpath(__file__))+'\\onda.gif', writer='imagemagick')
---
+++ fdtd3d.py
@@ -0,0 +1,47 @@
+from inspect import isfunction
+from math import sqrt, sin, pi
+
+mi0 = 4e-7 * pi
+eps0 = 8.854187817e-12
+
+def fdtd_gen(i_max, delta, delta_t, coefs, f_meio = None, j_max=None,):
+	if j_max == None:
+		j_max = i_max
+	
+	Ez_ant = Hy_ant = Hx_ant = Ez = Hx = Hy = Jz_fonte = Mx_fonte = My_fonte = [[0 for i in range(0, i_max)] for j in range(0, j_max)]
+	
+	n = 0
+
+	while True:
+		yield Ez, Hx, Hy
+
+		n = n + 1
+
+		Ez_ant = Ez
+		Hx_ant = Hx
+		Hy_ant = Hy
+
+		Ez = (
+			[[0 for j in range (0, j_max)]]
+			+ [[0] + [passo_Ez(i, j, Ez_ant, Hy_ant, Hx_ant, Jz_fonte, delta, coefs) for j in range(1, j_max - 1)] + [0] for i in range(1, i_max - 1)]
+			+ [[0 for j in range(0, j_max)]]
+		)
+
+		if isfunction(f_meio):
+			Ez[round(i_max/2)][round(j_max/2)] = f_meio(n*delta_t)
+
+		Hx = [[0] + [passo_Hx(i, j, Hx_ant, Ez, Mx_fonte, delta, coefs) for j in range(1, j_max)] for i in range(0, i_max)]
+
+		Hy = (
+			[[passo_Hy(i, j, Hy_ant, Ez, My_fonte, delta, coefs) for j in range(0, j_max)] for i in range(0, i_max - 1)]
+			+ [[0 for j in range(0, j_max)]]
+		)
+
+def passo_Ez(i, j, Ez_ant, Hy_ant, Hx_ant, Jz_fonte, delta, coefs):
+	return coefs[i][j]['Ca'] * Ez_ant[i][j] + coefs[i][j]['Cb'] * (Hy_ant[i][j] - Hy_ant[i-1][j] + Hx_ant[i][j] - Hx_ant[i][j+1] - Jz_fonte[i][j]*delta)
+
+def passo_Hx(i, j, Hx_ant, Ez, Mx_fonte, delta, coefs):
+	return coefs[i][j]['Da']*Hx_ant[i][j] + coefs[i][j]['Db'] * (Ez[i][j-1] - Ez[i][j] - Mx_fonte[i][j]*delta)
+
+def passo_Hy(i, j, Hy_ant, Ez, My_fonte, delta, coefs):
+	return coefs[i][j]['Da']*Hy_ant[i][j] + coefs[i][j]['Db'] * (Ez[i+1][j] - Ez[i][j] - My_fonte[i][j]*delta)
